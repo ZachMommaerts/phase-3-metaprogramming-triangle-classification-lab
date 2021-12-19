be0134c500a397f2a2094eb2f6bd1c76d553edd8
Zach <zmommaerts@gmail.com>
2021-12-19 00:00:10 -0600

    commit tree 2fde2c02dc43c273a4f8c0e6ff9d9ce736c35481

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+  
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    if one > 0 && two > 0 && three > 0 && (one + two) > three && (one + three) > two && (two + three) > one
+      if one == two && two == three
+        :equilateral
+      elsif one == two || two == three || one == three
+        :isosceles
+      elsif one != two && two != three || one != three
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      'that aint no triangle'
+    end
+  end
 end
